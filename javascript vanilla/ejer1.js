fb7ff029adb3a508a43c31fe55f65e12ea94e2b1
Alberto Amo <alberto.amo.marin@hp.com>
2022-09-15 13:56:37 +0200

    ejer1

--- javascript vanilla/ejer1.js
+++ javascript vanilla/ejer1.js
@@ -1,1 +1,10 @@
-console.log('hello');
+let var1 = 1;
+let var2 = 3;
+
+if(var1 < var2) {
+    console.log('el mayor es', var2);
+}
+
+if(var1 == var2) {
+    console.log('valen lo mismo var1:',var2, ' var2:', var2);
+}
