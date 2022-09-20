7720e42882a06a0ea173fc2903cb28a704960244
Alberto Amo <alberto.amo.marin@hp.com>
2022-09-20 17:06:08 +0200

    fix borrar y modificar

--- angular/admon-articulos/src/app/app.component.ts
+++ angular/admon-articulos/src/app/app.component.ts
@@ -49,10 +49,22 @@ export class AppComponent implements OnInit {
       (item) => item.codigo === codigo
     ) as articulo;
 
-    this.articuloMostrar = {...valor};
+    this.articuloMostrar = { ...valor };
   }
 
-  addArticulo(codigo: string, descripcion: string, precio: number) {}
+  addArticulo(codigo: string, descripcion: string, precio: number) {
+    let encontrado = false;
+    for (let articulo of this.articulos) {
+      console.log(articulo.codigo,codigo);
+      if (articulo.codigo === codigo) {
+        encontrado = true;
+      }
+    }
+
+    if(!encontrado) {
+      this.articulos.push({codigo,descripcion,precio});
+    }
+  }
 
   modify() {
     console.log('entro en modify');
@@ -60,5 +72,13 @@ export class AppComponent implements OnInit {
       (x) => x.codigo == this.articuloMostrar.codigo
     );
     this.articulos[index] = this.articuloMostrar;
+  }
+
+  borrar(codigo:string) {
+    let index = this.articulos.findIndex(
+      (x) => x.codigo == this.articuloMostrar.codigo
+    );
+
+    this.articulos.splice(index,1)
   }
 }
