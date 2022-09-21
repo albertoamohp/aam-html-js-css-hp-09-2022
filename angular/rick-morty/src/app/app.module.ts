8297a9da0ff0c15e27afe7c18eac64023b9ce29d
Alberto Amo <alberto.amo.marin@hp.com>
2022-09-21 09:26:09 +0200

    componentes y bootsrap

--- angular/rick-morty/src/app/app.module.ts
+++ angular/rick-morty/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PersonajesComponent } from './personajes/personajes.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PersonajesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
