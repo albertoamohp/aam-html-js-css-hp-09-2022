d2d2a507ca70ae284c3a8e373b0d2bf5fa7157d3
Alberto Amo <alberto.amo.marin@hp.com>
2022-09-22 10:10:39 +0200

    little things

--- angular/rick-morty/src/app/app-routing.module.ts
+++ angular/rick-morty/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PersonajesComponent } from './personajes/personajes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'characters', component: PersonajesComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
