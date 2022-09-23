d1b93a3eecbcf90993c728b1cf5e590ee67112d1
Alberto Amo <alberto.amo.marin@hp.com>
2022-09-23 14:16:32 +0200

    add components and services

--- angular/books/src/app/app-routing.module.ts
+++ angular/books/src/app/app-routing.module.ts
@@ -1,27 +1,23 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AddBookComponent } from './components/add-book/add-book.component';
-import { BookDetailsComponent } from './components/book-details/book-details.component';
-import { BookListComponent } from './components/book-list/book-list.component';
+import { Routes, RouterModule } from '@angular/router';
+
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'books',
-    pathMatch: 'full',
-  },
-  {
-    path: 'books',
-    component: BookListComponent,
-  },
-  {
-    path: 'books/:id',
-    component: BookDetailsComponent,
-  },
-  {
-    path: 'add',
-    component: AddBookComponent,
-  },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'user', component: BoardUserComponent },
+  { path: 'mod', component: BoardModeratorComponent },
+  { path: 'admin', component: BoardAdminComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
--- angular/books/src/app/app.module.ts
+++ angular/books/src/app/app.module.ts
@@ -8,13 +8,27 @@ import { AddBookComponent } from './components/add-book/add-book.component';
 import { BookDetailsComponent } from './components/book-details/book-details.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { BoardUserComponent } from './board-user/board-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddBookComponent,
     BookDetailsComponent,
-    BookListComponent
+    BookListComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    BoardAdminComponent,
+    BoardModeratorComponent,
+    BoardUserComponent
   ],
   imports: [
     BrowserModule,
